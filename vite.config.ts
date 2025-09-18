558a69416fb5a6e67d0ed3041ea9a81dedfffd00
BONU LOVAKUMAR <lovakumarbonu@gmail.com>
2025-09-18 10:18:39 +0530

    Delete vite.config.ts

--- vite.config.ts
+++
@@ -1,9 +0,0 @@
-
-import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  plugins: [react()],
-  base: "/CROP-DISEASE-DETECTOR/",
-})
